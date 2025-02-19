264ba46bf7beb6ed677f6921368c5f0b656601f8
joel body <joel.body@proton.me>
2025-02-19 14:21:25 +0000

    added function to take input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    let key = input();
+    println!("{}", key);
+}
+
+fn input() -> String {
+    //reading input from the console
+    let mut text = String::new();
+    stdin().read_line(&mut text).expect("Error reading input from console");
+    text.pop();
+    text
 }
