15684a785f75ab928da3d9ac9d1e21303c315426
joel body <joel.body@proton.me>
2025-02-19 14:34:26 +0000

    read values for key, action & parameters. Formatted them with SOCKET to form query

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
+//example api link: http://192.168.1.120:7878/seckey/delete/foo.test
 use std::io::stdin;
 
 fn main() {
-    let key = input();
-    println!("{}", key);
+    let key = input("Enter the key: ");
+    let action = input("Enter the desired action");
+    let parameters = input("Enter the desired parameters");
+
+    let query = format!("{SOCKET}/{key}/{action}/{parameters}");
+    println!("query: {}", query);
 }
 
-fn input() -> String {
+fn input(question: &str) -> String {
+    println!("{}", question);
     //reading input from the console
     let mut text = String::new();
-    stdin().read_line(&mut text).expect("Error reading input from console");
+    stdin()
+        .read_line(&mut text)
+        .expect("Error reading input from console");
     text.pop();
     text
 }
+
+//change this for deployment. Possibly read from .yaml config file.
+const SOCKET: &str = "localhost:7878";
