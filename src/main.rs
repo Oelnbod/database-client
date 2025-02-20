411b802e332841e46bf686c2a59e799890443b49
joel body <joel.body@proton.me>
2025-02-20 15:54:50 +0000

    encrypted passwords

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 //example api link: http://192.168.1.120:7878/seckey/delete/foo.test
+use base64;
 use reqwest::{get, Error};
 use std::io::stdin;
 use tokio;
-
+use aes_gcm::{
+    aead::{Aead, AeadCore, KeyInit, OsRng},
+    Aes256Gcm, Key, Nonce
+};
+use std::str;
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let key = input("Enter the key: ");
+    let aes_key = "thiskeystrmustbe32charlongtowork".to_string();
     loop {
 	println!("\n**Actions availiable**
 list_all
@@ -21,12 +26,15 @@ delete\n");
                 let website = input("Enter the website: ");
                 let username = input("Enter the Username/email: ");
                 let password = input("Enter the password: ");
+		let password = encrypt(aes_key.clone(), password);
+		
                 &format!("{website},{username},{password}").to_string()
             }
             "delete" => &input("Enter the website of the row that you wish to delete: "),
             _ => "",
         };
-        let query = format!("http://{SOCKET}/{key}/{action}/{parameters}");
+	
+	let query = format!("http://{SOCKET}/{KEY}/{action}/{parameters}");
         println!("query: {}", query);
 
         let result = get(query).await?.text().await?;
@@ -43,6 +51,24 @@ fn input(question: &str) -> String {
     text.pop();
     text
 }
+fn encrypt(key_str: String, plaintext: String) -> String {
+    let key = Key::<Aes256Gcm>::from_slice(key_str.as_bytes());
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+
+    let cipher = Aes256Gcm::new(key);
+
+    let ciphered_data = cipher.encrypt(&nonce, plaintext.as_bytes())
+	.expect("err");
+
+    let mut encrypted_data: Vec<u8> = nonce.to_vec();
+    encrypted_data.extend_from_slice(&ciphered_data);
+
+    let string_encrypted_data = base64::encode(encrypted_data);
+    string_encrypted_data
+    
+}
+
 
 //change this for deployment. Possibly read from .yaml config file.
 const SOCKET: &str = "localhost:7878";
+const KEY: &str = "seckey"; //this is the api key
